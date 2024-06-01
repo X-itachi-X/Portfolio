9449b9fe2ba96f97eb2f5f1068e900054797206b
X-itachi-X <ankitprakash2909@gmail.com>
2024-06-01 11:27:03 +0530

    Speed insight

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import {FloatingNav} from "@/components/ui/FloatingNav"
 import { navItems } from "@/data";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <SpeedInsights />
       <div className="max-w-7xl w-full">
         <FloatingNav 
           navItems={navItems}
